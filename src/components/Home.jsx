9291bfbfe86384ce625c4703d0fda16acb4a6cba
Roopak Mahadane <roopakmahadane@users.noreply.github.com>
2025-07-25 19:34:15 +0530

    About me added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import "../App.css";
 import Header from "./Header";
 import Cover from "./Cover";
+import AboutMe from "./AboutMe";
 
 export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen font-inter text-gray-800">
       <Header />
-        <Cover />
+        <Cover className="min-h-screen"/>
+        <AboutMe />
       
     </div>
   );
