df24c1dfd54f06b9a4524178c8a40916b67596c8
Roopak Mahadane <roopakmahadane@users.noreply.github.com>
2025-07-31 19:18:53 +0530

    Added connect

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,7 +1,15 @@
 export default function Connect(){
     return (
-        <div className="bg-[#121212] mt-40 p-6 mx-auto max-w-6xl  font-inter">
-        <h1 className="text-4xl font-bold align-center text-center text-[#ffffff]  mb-4 mb-10">Get in touch</h1>
+        <div className="bg-[#121212] mt-40 p-6 mx-auto max-w-6xl  font-inter flex flex-col items-center"> 
+            <h1 className="text-3xl font-bold text-[#ffffff] mb-6">Let's Connect</h1>
+            <p className="text-gray-400 text-lg mb-8">I'm always open to discussing new projects, creative ideas, or opportunities to be part of your vision.</p>
+       <div/>
+       <div className="md:w-35 h-12 bg-[#bb86fc]  flex justify-center rounded-sm">
+       <a href="mailto:roopak.mhdn@gmail.com" className="w-full bg-[#121212] text-center text-[#bb86fc] border p-2 rounded-sm origin-bottom-right transform hover:-translate-x-1 hover:-translate-y-1 border-[#bb86fc] transition ease-in-out duration-300 cursor-pointer" target="_blank" rel="noopener noreferrer">
+    Get in Touch
+</a>
+       </div>
+        
         </div>
     )
 }
