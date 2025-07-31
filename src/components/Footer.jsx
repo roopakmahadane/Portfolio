17c7a62f4722daafb7f75c7ba141c0f0e7fd4de1
Roopak Mahadane <roopakmahadane@users.noreply.github.com>
2025-07-31 19:31:14 +0530

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+return (
+    <div className="flex flex-col items-center justify-center bg-[#121212] text-white p-5 font-inter  mt-40">
+        <p className=" text-gray-400">Built by <a target="_blank" className="text-blue-500" href="https://github.com/roopakmahadane">Roopak Mahadane</a>  </p>  
+        <p className="text-gray-400">Design inspiration from  <a target="_blank" className="text-blue-500" href="https://brittanychiang.com/">brittanychiang</a></p>
+    </div>
+)
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,16 +5,18 @@ import AboutMe from "./AboutMe";
 import Projects from "./Projects";
 import MoreProjects from "./MoreProjects";
 import Connect from "./Connect";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
-    <div className="bg-[#121212]  font-inter text-[##ffffff]">
+    <div className="bg-[#121212]  font-inter text-[#ffffff]">
       <Header />
         <Cover className="min-h-screen"/>
         <AboutMe />
         <Projects />
         <MoreProjects />
         <Connect />
+        <Footer />
     </div>
   );
 }
